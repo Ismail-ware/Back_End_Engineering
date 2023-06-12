a44b7fadd943b3a4af7f3380bdd76bdd7fa2eb30
Ismail Dele Wakilu <85701526+Ismail-ware@users.noreply.github.com>
2023-06-12 18:43:47 +0100

    Update Back_End_Engineering_Solution.py

--- Back_End_Engineering_Solution.py
+++ Back_End_Engineering_Solution.py
@@ -1,83 +1,81 @@
-#import relevant libraries
 import requests
 import json
-import pandas as pd
+import logging
 
-# Set the base URL and API key
 base_url = "https://api.krakenflex.systems/interview-tests-mock-api/v1/"
-API_KEY = "EltgJ5G8m44IzwE6UN2Y4B4NjPW77Zk6FJK3lL23"
+API_KEY = "12224"
 Site_ID = "norwich-pear-tree"
 
-#create a helper function `main_request` for reusability
 def main_request(method, endpoint, params=None, data=None):
     headers = {"x-api-key": API_KEY}
     url = base_url + endpoint
 
-    response = requests.request(method, url, params=params, data=data, headers=headers)
-
-    print("Response Status Code:", response.status_code)
-    print("Response Content:", response.content)
-
-    if response.status_code == 200:
+    try:
+        response = requests.request(method, url, params=params, data=data, headers=headers)
+        response.raise_for_status()
         return response.json()
-    else:
-        raise Exception(f"Request failed with status code {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Request failed: {str(e)}")
+        raise
 
-## Task I
-outages = main_request("GET", "outages")
-indented_outages = json.dumps(outages, indent=2) #indenting the output for better visualization
-print(indented_outages)
+def get_outages():
+    return main_request("GET", "outages")
 
-## Task II
-site_info = main_request("GET", f"site-info/{Site_ID}")
-indented_site_info = json.dumps(site_info, indent=2) ##indenting the output for better visualization
-print(indented_site_info)
+def get_site_info(site_id):
+    endpoint = f"site-info/{site_id}"
+    return main_request("GET", endpoint)
 
-## Task III
-devices = site_info["devices"]  ##subsetting devices from `site_info`
-print(devices)
+def filter_outages(outages, devices):
+    filtered_outages = []
+    for outage in outages:
+        if outage['begin'] < '2022-01-01T00:00:00.000Z' or outage['id'] not in [device['id'] for device in devices]:
+            filtered_outages.append(outage)
+    return filtered_outages
 
-#filtering outages with a start time less than `2022` or whose `id` is not in devices
-filtered_outages = [outage for outage in outages
-                    if (outage['begin'] < '2022-01-01T00:00:00.000Z') or (outage['id'] not in [device['id'] for device in devices])]
+def get_outages_after_2022_in_device(outages, devices):
+    outages_after_2022 = []
+    for outage in outages:
+        if outage['begin'] >= '2022-01-01T00:00:00.000Z' and outage['id'] in [device['id'] for device in devices]:
+            outages_after_2022.append(outage)
+    return outages_after_2022
 
-print(filtered_outages)
+def attach_device_names(outages, devices):
+    for outage in outages:
+        for device in devices:
+            if device['id'] == outage['id']:
+                outage['device_name'] = device['name']
+                break
+    return outages
 
-## Task IV
-#filtering outages after 2022 whose `id` are in devices
-outages_after_2022_in_devices = [outage for outage in outages
-                    if (outage['begin'] >= '2022-01-01T00:00:00.000Z') and (outage['id'] in [device['id'] for device in devices])]
+def transform_outages(outages):
+    transformed_outages = []
+    for outage in outages:
+        transformed_outage = {
+            "id": outage['id'],
+            "name": outage['device_name'],
+            "begin": outage['begin'],
+            "end": outage['end']
+        }
+        transformed_outages.append(transformed_outage)
+    return transformed_outages
 
-print(outages_after_2022_in_devices)
+def post_outages(site_id, outages):
+    endpoint = f"site-outages/{site_id}"
+    data = json.dumps(outages)
+    return main_request("POST", endpoint, data=data)
 
-#appending `device_name` to `outages_after_2022_in_device`
-remaining_outages = []
-for outage in outages_after_2022_in_devices:
-    outage_id = outage['id']
-    for device in devices:
-        if device['id'] == outage_id:
-            outage['device_name'] = device['name']
-            break
-    remaining_outages.append(outage)
+def run_task():
+    outages = get_outages()
+    site_info = get_site_info(Site_ID)
+    devices = site_info.get("devices", [])
 
-print(remaining_outages)
+    filtered_outages = filter_outages(outages, devices)
+    outages_after_2022 = get_outages_after_2022_in_device(outages, devices)
+    remaining_outages = attach_device_names(outages_after_2022, devices)
+    transformed_outages = transform_outages(remaining_outages)
 
-## Task V
-#modifying data schema to conform to the payload requirement
-filtered_outages = []
-for outage in remaining_outages:
-    filtered_outage = {
-        "id": outage['id'],
-        "name": outage['device_name'],
-        "begin": outage['begin'],
-        "end": outage['end']
-    }
-    filtered_outages.append(filtered_outage)
+    response = post_outages(Site_ID, transformed_outages)
+    print(response)
 
-print(filtered_outages)
-
-# Convert filtered_outages to JSON
-filtered_outages_json = json.dumps(filtered_outages)
-print(filtered_outages_json)
-
-main_request("POST", f"site-outages/{Site_ID}", data=filtered_outages_json)
+if __name__ == '__main__':
+    run_task()
